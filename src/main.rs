33a8e93e342548a5a90f0f2c4c01964d7b695ef3
Graham Knapp <graham.knapp@acernis.fr>
2023-01-26 20:09:55 +0100

    Implement Task struck

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+struct Task {
+    description: String,
+    complete: bool,
+}
+
+impl Task {
+    fn new(description: &str) -> Self {
+        Self {
+            description: String::from(description),
+            complete: false,
+        }
+    }
+}
+
 fn main() {
-    println!("Todo: Write a todo app!");
+    let tasks = vec![Task::new("Todo: Write a todo app!")];
+    for task in tasks { if !task.complete { println!("⬛{}", task.description) } }
+    ;
 }
