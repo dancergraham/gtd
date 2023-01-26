b779fe93b03e872ca962c837960e0f1c8010d2d5
Graham Knapp <graham.knapp@acernis.fr>
2023-01-26 20:13:33 +0100

    Todo: add todo list

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,12 @@ impl Task {
 }
 
 fn main() {
-    let tasks = vec![Task::new("Todo: Write a todo app!")];
+    let tasks = vec![Task::new("Todo: Write a todo app!"),
+                     Task::new("Accept user input"),
+                     Task::new("Allow tasks to be marked complete"),
+                     Task::new("Save DB"),
+                     Task::new("Add Gui"),
+    ];
     for task in tasks { if !task.complete { println!("⬛{}", task.description) } }
     ;
 }
